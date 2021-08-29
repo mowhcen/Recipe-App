125b498d9d24c61faaf3f7a0ad43f7a25c9cf62d
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-08-29 15:35:46 +0430

    feat: make data save to localstorage variables

---
+++ src/recipe.js
@@ -0,0 +1,65 @@
+import uuidv4 from "uuid/v4";
+
+/**
+ * make value for saving recipe in local storage
+ */
+let recipes = [];
+
+/**
+ * Read data from local storage if is there any
+ */
+const loadRecipe = () => {
+    const recipeJSON = localStorage.getItem("recipe");
+
+    try {
+        return recipeJSON ? JSON.parse(recipeJSON) : [];
+    } catch (e) {
+        return [];
+    }
+};
+
+/**
+ * store recipe to localStorage
+ */
+const storeRecipe = () => {
+    localStorage.setItem("recipe", JSON.stringify(recipes));
+};
+/**
+ * return recipe
+ */
+const getRecipe = () => recipes;
+/**
+ * create note object
+ * @returns id
+ */
+const createRecipe = () => {
+    const id = uuidv4();
+    recipes.push({
+        id: id,
+        title: "",
+        instruction: "",
+        ingredients: [],
+    });
+
+    return id;
+};
+/**
+ * find a recipe and remove it by its id
+ */
+const removeRecipe = (id) => {
+    const recipeIndex = recipes.findIndex((recipe) => recipe.id === id);
+
+    if (recipeIndex > -1) {
+        recipes.splice(recipeIndex, 1);
+        storeRecipe();
+    }
+};
+/**
+ * load recipe to its variables
+ */
+recipes = loadRecipe();
+
+/**
+ * export function for use
+ */
+export { loadRecipe, storeRecipe, getRecipe, createRecipe, removeRecipe };
