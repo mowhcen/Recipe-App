b2b3c621fe18f8fc9eae95ffb3ffe202f9b07709
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-08-29 15:38:15 +0430

    style: add comment to code for better reading

--- src/filter.js
+++ src/filter.js
@@ -1,13 +1,27 @@
+/**
+ * filter input value of searchbar
+ */
 const filter = {
     textSearch: "",
 };
 
+/**
+ *
+ * @returns filter object
+ */
 const getFilter = () => filter;
 
+/**
+ * change filter object value
+ * @param {*} param0
+ */
 const setFilter = ({ textSearch: update }) => {
     if (typeof update === "string") {
         filter.textSearch = update;
     }
 };
 
+/**
+ * export function for use
+ */
 export { getFilter, setFilter };
