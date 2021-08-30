ff72c5faf3675fc85d0aea606dd018ec93b33c6c
Mohammad Mohsen Hosseini <mowhcen@gmail.com>
2021-08-30 12:38:56 +0430

    feat: add eventlistner for sumbit the form

--- src/edit.js
+++ src/edit.js
@@ -1,12 +1,17 @@
-import { removeRecipe, updateRecipe } from "./recipe";
+import { createIngredients, removeRecipe, updateRecipe } from "./recipe";
+import { printIngredients, showEditPage } from "./views";
 
 /**
  * create edit page element
  */
 const titleEl = document.querySelector("#recipe-title");
 const instructionEl = document.querySelector("#recipe-ins");
+const addIngredientEl = document.querySelector("#add-ing");
 const removeEl = document.querySelector("#remove");
 const recipeId = location.hash.substring(1);
+
+showEditPage(recipeId);
+printIngredients(recipeId);
 
 titleEl.addEventListener("change", (e) => {
     updateRecipe(recipeId, {
@@ -18,6 +23,13 @@ instructionEl.addEventListener("change", (e) => {
     updateRecipe(recipeId, {
         instruction: e.target.value,
     });
+});
+
+addIngredientEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createIngredients(recipeId, e.target.elements.title.value);
+    printIngredients(recipeId);
+    e.target.elements.title.value = "";
 });
 
 removeEl.addEventListener("click", () => {
